a2c35b091bddf15eb353293a962f8f632dcf746a
Tamandani-V <tamandani.victor@gmail.com>
2025-08-13 17:04:05 +0300

    Add files via upload

---
+++ MySQL Tama.session.sql
@@ -0,0 +1,11 @@
+-- Creating a new database called salesDB
+CREATE DATABASE salesDB;
+-- Select the line, right click, run the query independently
+-- Database "saleDB" created successfuly
+-- A second query run brings an error indicating the database "salesDB" already exists.
+
+
+-- Dropping Database called demo
+DROP DATABASE demo;
+-- Select the line, right click and run the query independently
+-- The query run brings an error "can't drop database 'demo'; database doesn't exist"
